1f9543782a2a8dedc17e6805ed8d59e3076327c5
Rachit123kumar <hellobittukumar12@gmail.com>
2025-05-01 19:39:05 +0530

    fixing route

--- app/api/uploadfile/route.js
+++ app/api/uploadfile/route.js
@@ -1,23 +1,23 @@
-import { NextResponse } from "next/server";
-import bodyParser from 'body-parser'
-import fileUpload from "express-fileupload"
+// import { NextResponse } from "next/server";
+// import bodyParser from 'body-parser'
+// import fileUpload from "express-fileupload"
 
-export const config={
-  api:{
-    bodyParser:false
-  }
-}
+// export const config={
+//   api:{
+//     bodyParser:false
+//   }
+// }
 
 
 
-export async function POST(req){
+// export async function POST(req){
 
-  const files=req.files;
+//   const files=req.files;
   
-console.log(files)
+// console.log(files)
 
-  return NextResponse.json({
-  message:"I am archit ",
-  files
-  })
-}
+//   return NextResponse.json({
+//   message:"I am archit ",
+//   files
+//   })
+// }
